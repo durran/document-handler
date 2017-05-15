fa77e8b1fcb46fcd8a0a80d11371d86031c80092
Durran Jordan <durran@gmail.com>
2017-05-15 17:42:47 -0400

    Handling initial set interception

---
+++ index.js
@@ -0,0 +1,3 @@
+module.exports = require('./lib/document-handler');
+module.exports.DocumentHandler = require('./lib/document-handler');
+module.exports.PropertyState = require('./lib/property-state');
---
+++ test/document-handler.test.js
@@ -0,0 +1,63 @@
+const { expect } = require('chai');
+const { DocumentHandler, PropertyState } = require('../');
+
+const OLD = 'old name';
+const NEW = 'new name';
+
+describe('DocumentHandler', () => {
+  describe('#set', () => {
+    context('when the property does not yet exist', () => {
+      const doc = {};
+      const handler = new DocumentHandler();
+      const proxy = new Proxy(doc, handler);
+
+      before(() => {
+        proxy.name = NEW;
+      });
+
+      it('sets the property on the proxy', () => {
+        expect(proxy.name).to.equal(NEW);
+      });
+
+      it('sets the property on the target', () => {
+        expect(doc.name).to.equal(NEW);
+      });
+
+      it('flags the property as added', () => {
+        expect(handler.states.name.name).to.equal(PropertyState.ADDED);
+      });
+    });
+
+    context('when the property exists', () => {
+      context ('when the value is changed', () => {
+        const doc = { name: OLD };
+        const handler = new DocumentHandler();
+        const proxy = new Proxy(doc, handler);
+
+        before(() => {
+          proxy.name = NEW;
+        });
+
+        it('sets the property on the proxy', () => {
+          expect(proxy.name).to.equal(NEW);
+        });
+
+        it('sets the property on the target', () => {
+          expect(doc.name).to.equal(NEW);
+        });
+
+        it('flags the property as edited', () => {
+          expect(handler.states.name.name).to.equal(PropertyState.EDITED);
+        });
+
+        it('sets the original value on the state', () => {
+          expect(handler.states.name.originalValue).to.equal(OLD);
+        });
+      });
+
+      context('when the value is not changed', () => {
+
+      });
+    });
+  });
+});
